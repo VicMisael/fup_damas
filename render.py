ae12debb80dcc188aabb13cbb9024c984de65af8
Victor Misael <misael1805@gmail.com>
2019-05-22 13:27:14 -0300

    Update render.py
    the board is now working

--- render.py
+++ render.py
@@ -1,28 +1,35 @@
 def separar():
+    print("",end=" ")
     for i in range(21):
         if(i%2==0):
-            print("+",end=" ")
+            print("+",end="")
         else:
-            print("-",end=" ")
+            print("-",end="")
     #print("\n")
 def desenharLetras():
-    print("")
     letras=["A","B","C","D","E","F","G","H","I","J"]
     c=0;
+    print("  ",end='')
     for i in range(20):
-        print("",end=" ")
         if(i%2==1):
             print(letras[c],end=" ")
             c+=1;
-    print("")
+    print()
 def renderizar():
     
     desenharLetras()    
     #print("")  
-    #separar()   
+    separar()
+    print()   
     for y in range(10):
+        print(y,end="")
+        
+        for x in range(10):
+            print("| ",end="")
+            
+        print("|")
         separar()   
-       
+        print() 
        
         
     desenharLetras()
